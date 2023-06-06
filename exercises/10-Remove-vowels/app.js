ce63df8e29777b0ffa38879c7cf248c8613a5ad8
Jose1gnacio <104277952+Jose1gnacio@users.noreply.github.com>
2023-06-06 16:54:23 +0000

    final2

--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,17 +1,16 @@
 // Your code goes here
-const rapid =(text) =>{
-    let string = text.toLowerCase() 
-    for (let i=0; i<string.length; i++) {
-        const element = string[i];
-        if (element === "a" || element === "e" ||element === "i" ||element === "o" || element === "u" ) {
-            
-        } else {
-            string.toUpperCase()
-        }
-        return text2 = 
+const rapid = (text) => {
+    let string = text.toLowerCase();
+    let consonantes = "";
+    
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] !== "a" && string[i] !== "e" && string[i] !== "i" && string[i] !== "o" && string[i] !== "u") {
+        consonantes += string[i].toUpperCase();
+      }
     }
-
-}
+    
+    return consonantes;
+  }
 
 // Work above this line; do not change code below
 let str = "John";
